54f2af8e223226f00fa1632b2843b82eb4b92f82
Tiago Barbosa <tiagobarbosa98@gmail.com>
2019-05-24 20:57:13 +0100

    int1 atualizado

--- projetoTerceiraParte/Queries/int1.sql
+++ projetoTerceiraParte/Queries/int1.sql
@@ -12,7 +12,19 @@ group by Filme.idVideo
 order by ac desc
 limit 5;
 
+Create View avgByIdd as
+Select distinct Filme.idVideo, avg(classificacao) as ac
+From Visualiza, Filme
+Where Filme.idVideo = Visualiza.idVideo
+group by Filme.idVideo
+order by ac asc
+limit 5;
+
 Select titulo as 'Filmes com melhor classificacao', avgById.ac as 'Classificacao Media'
 From Video, avgById
 Where Video.idVideo = avgById.idVideo;
+
+Select titulo as 'Filmes pior classificacao', avgByIdd.ac as 'Classificacao Media'
+From Video, avgByIdd
+Where Video.idVideo = avgByIdd.idVideo;
 
