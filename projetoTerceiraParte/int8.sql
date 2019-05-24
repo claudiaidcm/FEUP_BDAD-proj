4c073a543b8c681e87e3075b90375993e00f2d82
Tiago Barbosa <tiagobarbosa98@gmail.com>
2019-05-24 04:09:20 +0100

    Fixing merge errors

---
+++ projetoTerceiraParte/int8.sql
@@ -0,0 +1,9 @@
+.mode columns
+.headers on
+.nullvalue NULL
+
+-- Titulos dos filmes realizados por BELLA em 2019
+SELECT titulo AS 'TITULO'
+from Video
+where (realizador = 'Bella')
+AND strftime('%Y', dataPublicacao) = '2019'
